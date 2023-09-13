262201e6440f24dc8906caea221558dc46af1fb7
Kennedy Nwaorgu <devjckennedy@gmail.com>
2023-09-13 14:55:13 -0400

    Add multipage

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,14 +1,17 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using System.Collections.Generic;
 
 namespace Phoneword
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,6 +22,7 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
             translateButton.Click += (sender, e) =>
             {
                 // Translate user's alphanumeric phone number to numeric
@@ -30,7 +34,15 @@ namespace Phoneword
                 else
                 {
                     translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
                 }
+            };
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
             };
         }
         //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
