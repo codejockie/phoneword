c821aaaf379db2289241947cc9a6f106ac144644
Kennedy Nwaorgu <devjckennedy@gmail.com>
2023-09-19 11:10:48 -0400

    experiment with non configuration instance

--- MainActivity.cs
+++ MainActivity.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 
 namespace Phoneword
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
         static readonly List<string> phoneNumbers = new List<string>();
---
+++ NonConfigInstanceActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Util;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Phoneword
+{
+    [Activity(Label = "NonConfigInstanceActivity", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Obsolete]
+    public class NonConfigInstanceActivity : ListActivity
+    {
+        DogListWrapper _savedInstance;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            if (LastNonConfigurationInstance is DogListWrapper dogsWrapper)
+            {
+                PopulateDogList(dogsWrapper.Dogs);
+                Log.Debug(GetType().FullName, "LastNonConfigurationInstance");
+            }
+            else
+            {
+                GetDogBreeds();
+                Log.Debug(GetType().FullName, "API Call");
+            }
+        }
+
+        public override Java.Lang.Object OnRetainNonConfigurationInstance()
+        {
+            base.OnRetainNonConfigurationInstance();
+            return _savedInstance;
+        }
+
+        public void GetDogBreeds()
+        {
+            using var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://dog.ceo/api/"),
+            };
+
+            var result = httpClient.GetAsync("breeds/list/all").Result;
+            ParseResults(result);
+        }
+
+        void ParseResults(HttpResponseMessage httpRes)
+        {
+            var s = httpRes.Content.ReadAsStringAsync().Result;
+            var j = JsonNode.Parse(s);
+            var results = JsonSerializer.Deserialize<Dictionary<string, string[]>>(j["message"].ToJsonString()).Keys.ToArray();
+            RunOnUiThread(() => {
+                PopulateDogList(results);
+            });
+        }
+
+        void PopulateDogList(string[] results)
+        {
+            ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.item_view, results);
+            _savedInstance = new DogListWrapper(results);
+        }
+    }
+
+    class DogListWrapper : Java.Lang.Object
+    {
+        public string[] Dogs { get; set; }
+        public DogListWrapper(string[] dogs)
+        {
+            Dogs = dogs;
+        }
+    }
+}
